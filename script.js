d24bf2d85df506af17e818fe57db74778b6dac17
LACRIM94 <anasmestour@gmail.com>
2025-11-11 12:35:56 +0100

    Begin to work on the launch section in the home page

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ fetch('https://api.spacexdata.com/v5/launches')
     .then((result) => {
         console.log(result)
     })
+
+/* för eventet gör så att man kan trycka på en knapp som gör att den visar om
+launches va succesful eller inte */
